9fabf9f30cc65c4f7218c8319533b63fb5e4a46d
giusscos <giu.cosenza08@gmail.com>
2024-09-24 18:07:55 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Giuseppe Cosenza on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
