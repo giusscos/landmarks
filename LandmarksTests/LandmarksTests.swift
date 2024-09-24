d1f395bf6312f8e53285ed13d464e8a78dc527d5
giusscos <giu.cosenza08@gmail.com>
2024-09-24 18:07:55 +0200

    Initial Commit

---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by Giuseppe Cosenza on 24/09/24.
+//
+
+import Testing
+@testable import Landmarks
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
